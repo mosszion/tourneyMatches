9d831bcc93d513dae3c3a81ecc7fa68016b0f6a8
Moss2 <moss@Mosis-MacBook-Pro.local>
2024-06-24 18:50:38 -0400

    make parent component App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import MatchList from './components/MatchList.jsx';
+import PlayerList from './components/PlayerList.jsx';
+import matchData from './data/matchData.js';
+import playerData from './data/playerData.js';
+import { preparePlayerData, addWinsToPlayers } from "./helpers/playerHelpers";
+
 
 function App() {
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tourney Matches{" "} 
+        <span>Where Coding and Tournaments found their Match!</span>
+        </h1>
+      <PlayerList matchData = {matchData}  playerData={parsedPlayerData}/>
+      <MatchList matchData = {matchData} />
+     
     </div>
   );
 }
